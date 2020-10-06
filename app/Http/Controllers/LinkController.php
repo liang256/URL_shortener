9f27b57cc0b0a0ab1cf82a7e6fbfd59b92a3a1f3
‘LIANGYU <n795113@gmail.com>
2020-10-06 09:55:26 +0800

    v1

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -7,7 +7,6 @@ use App\Models\Link as Link;
 
 class LinkController extends Controller
 {
-	protected $id=10;
 
     public function redirect()
 	{
@@ -15,38 +14,27 @@ class LinkController extends Controller
 		$address = Link::where('id',$id)->firstOrFail()->original_adress;
 
 		return header("Location:". $address);
-		die();
 	}
 
-	public function store(Request $request)
+	public function store()
 	{
-		if(Link::where('original_adress',request('original_address'))!=Null)
+
+		if(Link::where('original_adress',request('original_address'))->first()!= Null)
 		{
 			$link = Link::where('original_adress',request('original_address'))->firstOrFail();
 
 			return view('input',[
 				'link'=>$link
 			]);
-		}
 
-
-		$link = Link::create([
+		}else{
+			$link = Link::create([
 			'original_adress'=>request('original_address')
-		]);
+			]);
 
-		return view('input',[
-			'link'=>$link
-		]);
-	}
-
-	function generateRandomString($length = 10) 
-	{
-	    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	    $charactersLength = strlen($characters);
-	    $randomString = '';
-	    for ($i = 0; $i < $length; $i++) {
-	        $randomString .= $characters[rand(0, $charactersLength - 1)];
-	    }
-	    return $randomString;
+			return view('input',[
+				'link'=>$link
+			]);
+		}
 	}
 }
