40fb339f960faefffbdf59691aaf9f107e570402
LIANGYU <n795113@gmail.com>
2020-11-12 15:28:06 +0800

    fix(db): Fix column typo

--- database/migrations/2020_09_29_100937_create_links_table.php
+++ database/migrations/2020_09_29_100937_create_links_table.php
@@ -15,7 +15,7 @@ class CreateLinksTable extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
-            $table->string('original_adress');
+            $table->string('original_address');
             $table->timestamps();
         });
     }
